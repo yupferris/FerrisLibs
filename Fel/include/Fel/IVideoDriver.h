5c04a0925e84ca0684d7d44d080f5097445b44d6
ferris <jake@outracks.com>
2013-05-18 16:14:51 +0200

    Updated Fel::IVideoDriver and vs project dependencies

--- Fel/include/Fel/IVideoDriver.h
+++ Fel/include/Fel/IVideoDriver.h
@@ -1,12 +1,16 @@
 #ifndef __FEL_IVIDEODRIVER_H__
 #define __FEL_IVIDEODRIVER_H__
 
+#include <Fgl.h>
+
 namespace Fel
 {
 	class IVideoDriver
 	{
 	public:
 		virtual ~IVideoDriver();
+
+		virtual void SetViewport(Fgl::Viewport *viewport) = 0;
 
 		virtual void SetOutput(int width, int height, const unsigned int *data) = 0;
 	};

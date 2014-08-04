dd15985f46fd5bd775b076ab749716c07f0e3e70
ferris <jake@outracks.com>
2014-08-04 15:46:44 +0200

    nice

--- Fplfs.Tests/RegexParserTests.fs
+++ Fplfs.Tests/RegexParserTests.fs
@@ -1,7 +1,12 @@
 ﻿module Fplfs.Tests.RegexPerserTests
+    open System
     open Xunit
     open FsUnit.Xunit
     open Fplfs.RegexParser
+
+    // TODO: Move to helpers when I have more test modules
+    let testSpecificException t f = (fun () -> f() |> ignore) |> should throw t
+    let testException f = testSpecificException typeof<Exception> f
 
     // Single character cases
     [<Fact>]
@@ -9,3 +14,7 @@
 
     [<Fact>]
     let ``Regex parse single character 01`` () = parseRegex "Q" |> should equal (CharAstNode 'Q')
+
+    // Error cases
+    [<Fact>]
+    let ``Regex parse empty string`` () = testException (fun () -> parseRegex "")
--- Fplfs/Fplfs.RegexParser.fs
+++ Fplfs/Fplfs.RegexParser.fs
@@ -6,4 +6,7 @@
         | OneOrMoreAstNode of RegexAstNode
         | ZeroOrOneAstNode of RegexAstNode*)
 
-    let parseRegex (s : string) = CharAstNode s.[0]
+    let parseRegex (s : string) =
+        match s.Length with
+        | 0 -> failwith "String must not be empty"
+        | _ -> CharAstNode s.[0]
